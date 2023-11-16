a3e88aedf7e4ca9df0698e7bbbf818c0b60cfe35
Gary3c <150540901+Gary3c@users.noreply.github.com>
2023-11-16 15:44:24 +0000

    reset

--- FileReader.java
+++ FileReader.java
@@ -1,10 +1,7 @@
-import java.util.ArrayList;
-
-interface FileReader {
-    /*
-    Can parse a new student csv file
-    Can parse a grade list csv file
-    Can parse other csv files…
-     */
-    public ArrayList<String> fileReader(String fileneame);
+public class FileReader {
+   /*
+   Can parse a new student csv file
+   Can parse a grade list csv file
+   Can parse other csv files…
+    */
 }
