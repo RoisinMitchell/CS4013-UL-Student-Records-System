093e23cba184aca5c1d21b13ca2d34e45b469528
Gary3c <150540901+Gary3c@users.noreply.github.com>
2023-11-17 21:35:23 +0000

    Module update

--- Module.java
+++ Module.java
@@ -1,23 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Module {
-
-    private String moduleCode;
-    private String moduleName;
-    private int moduleCredits;
-    private int qualityHours;
-
-    public Module(String code, String name, int credits, int qualityHours){
-        this.moduleCode = code;
-        this.moduleName = name;
-        this.moduleCredits = credits;
+    String code;
+    String name;
+    int credits;
+    int qualityHours;
+    HashMap<String, Double> gradeScale; //What ever data structure we want??
+    public Module(String code,String name,int credits,int qualityHours){
+        this.code = code;
+        this.name = name;
+        this.credits = credits;
         this.qualityHours = qualityHours;
     }
-
-    public String getModuleCode(){
-        return this.moduleCode;
+    public String getCode(){
+        return code;
     }
-
+    public void setCode(String code){
+        this.code= code;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setCredits(int credits){
+        this.credits = credits;
+    }
+    public int getCredits(){
+        return credits;
+    }
+    public int getQualityHours(){
+        return qualityHours;
+    }
+    public void setQualityHours(int qualityHours){
+        this.qualityHours = qualityHours; 
+    }
+    public HashMap<String, Double> getGradeScale(){
+        return gradeScale;
+    }
+    public void setGradeScale(HashMap<String,Double> gradeScale){
+        this.gradeScale = gradeScale;
+    }
     public String toString(){
-        return this.moduleCode + "," + this.moduleName +"," + this.moduleCredits + "," + this.qualityHours;
+        return this.code + "," + this.name +"," + this.credits + "," + this.qualityHours;
     }
-
 }
