b7da1a57518bed2ca2cceddb0937566e7d7a8bf2
Gary3c <150540901+Gary3c@users.noreply.github.com>
2023-11-21 15:42:05 +0000

    Student+menu changes

--- StudentRecordSystemMenu.java
+++ StudentRecordSystemMenu.java
@@ -6,12 +6,20 @@ public class StudentRecordSystemMenu {
     public StudentRecordSystemMenu(){
         in = new Scanner(System.in);
         system = new StudentRecordSystem();
+        try {
+        system.setModules("Modules.csv");
+        system.setCourses("Courses.csv");
+        system.setStudents("Students.csv");
+        } catch (IOException e){
+            System.out.println("Something Failed up");
+        };
         // further setup
     }
     public void run() throws IOException{
         boolean running = true;
         while(running){
-            System.out.println("1)Add Student Grades\n2)Transcripts\n3)Review \n4)Add Courses\n5)Quit" );
+            
+            System.out.println("1)Add Student Grades\n2)Transcripts\n3)Review \n4)Add Students\n5)Quit" );
             String command = in.nextLine();
             if(command.equals("1")){
                 System.out.println("Type Filename");
