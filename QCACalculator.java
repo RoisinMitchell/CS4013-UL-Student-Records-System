7b8c0f191a2a16f0679a9fabcdaaf0fa6805b6e2
Jinggggggg <135239199+Jinggggggg@users.noreply.github.com>
2023-11-22 11:30:35 +0000

    Added methods to Grade class, added methods to QCACalculator class

--- QCACalculator.java
+++ QCACalculator.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class QCACalculator {
     
@@ -9,42 +10,53 @@ public class QCACalculator {
      
 
     StudentRecordSystem recordSystem = new StudentRecordSystem();
-    private ArrayList<Grade> gradesList = recordSystem.getGrades();
-    private ArrayList<Module> modulesList = recordSystem.getModules();
-    
-    public double getQPV(String studentID) {
-        gradesList = recordSystem.getGrades();
-        double studentQPV = 0.0;
-        for (Grade grade : gradesList) {
-            if (grade.getStudentID().equals(studentID)) {
-                String studentGrade = grade.getGradeByID(studentID);
-                studentQPV = grade.getQPV(studentGrade);
-            }
-        }
-        return studentQPV;
+    private Grade grade;
+    private Module module;
+    private HashMap<Module,Grade> studentGrades;
+    private HashMap<String, Double> hashMapGrade; // A1 - 4.0...
+
+    public QCACalculator(HashMap<Module,Grade> studentGrades){
+        this.studentGrades = studentGrades;
+        getTheGradeTable();
     }
 
-    public int getModuleCreditsByID(String studentID){
-        int moduleCredits = 0;
-        String moduleCode = "";
-        for (Grade grade : gradesList) {
-            if (grade.getStudentID().equals(studentID)) {
-                moduleCode = grade.getModuleCodeByID(studentID);
-            }
-        }
-        for (Module module : modulesList) {
-            moduleCredits = module.getModuleCreditsByCode(moduleCode);
-        }
-        return moduleCredits;
+    public HashMap<String, Double> getTheGradeTable(){
+        hashMapGrade = new HashMap<String, Double>();
+        hashMapGrade.put("A1", 4.00);
+        hashMapGrade.put("A2", 3.60);
+        hashMapGrade.put("B1", 3.20);
+        hashMapGrade.put("B2", 3.00);
+        hashMapGrade.put("B3", 2.80);
+        hashMapGrade.put("C1", 2.60);
+        hashMapGrade.put("C2", 2.40);
+        hashMapGrade.put("C3", 2.00);
+        hashMapGrade.put("D1", 1.60);
+        hashMapGrade.put("F", 0.00);
+        hashMapGrade.put("NG", 0.00);
+        return hashMapGrade;
     }
 
-    public double calculateQCS(String studentID) {
-        return getQPV(studentID) * getModuleCreditsByID(studentID);
+    public int getCredits(){
+        return module.getModuleCredits();
     }
 
-    public double calculateQCA() {
-        // sumation of QCS/sumation of(AH-NQH)
-        return 1.0;
+    public int getQualityHours(){
+        return module.getQualityHours();
+    }
+
+
+    // QCS = QPV * Credits
+    public double calculateQCS(){
+        double summationQCS = 0.0;
+        for (Grade grade: studentGrades.values()){
+            summationQCS += hashMapGrade.get(grade.getGradeLetter()) * getCredits();
+        }
+        return summationQCS;
+    }
+
+    // semester QCA = summation(QCS)/summation(AH-NQH)
+    public double calculateQCA(){
+        return calculateQCS()/30;
     }
  
 }
