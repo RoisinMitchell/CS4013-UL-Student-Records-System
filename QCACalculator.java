6015b1255195806756c92cbffe221c153892e1cb
Jinggggggg <135239199+Jinggggggg@users.noreply.github.com>
2023-11-22 17:16:06 +0000

    Making changes to QCACalculator class, added new classes

--- QCACalculator.java
+++ QCACalculator.java
@@ -3,11 +3,6 @@ import java.util.HashMap;
 
 public class QCACalculator {
     
-     * Knows QPV
-     * Knows module credits
-     * Can Calculate QCS (QPV * Credits)
-     * Can calculate QCA
-     
 
     StudentRecordSystem recordSystem = new StudentRecordSystem();
     private Grade grade;
