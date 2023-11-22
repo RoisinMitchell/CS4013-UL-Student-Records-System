f5b733d5623dcf1c7f515277cb419913e20ba91b
RoisinMitchell <mitchell.roisin@gmail.com>
2023-11-22 11:53:55 +0000

    Added Classes for Interface and Inheritance
    Added department class
    Added PostGraduateProgramme Class
    Added Programme interface class
    Added UnderGraduateProgramme class

---
+++ Programme.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public interface Programme {
+    void addModules(ArrayList<Module> modules);
+    void addCourses(ArrayList<Course> courses);
+    void enrollStudents(ArrayList<Student> students);
+
+}
---
+++ UnderGraduateProgramme.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class UnderGraduateProgramme implements Programme{
+    @Override
+    public void addCourses(ArrayList<Course> courses) {
+
+    }
+
+    @Override
+    public void addModules(ArrayList<Module> modules) {
+
+    }
+
+    @Override
+    public void enrollStudents(ArrayList<Student> students){
+
+    }
+
+
+}
