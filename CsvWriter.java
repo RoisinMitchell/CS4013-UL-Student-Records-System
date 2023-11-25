84c6e5ad4866d4bf950311b4cf22012ed0232947
RoisinMitchell <mitchell.roisin@gmail.com>
2023-11-25 21:31:37 +0000

    Altered csv data, Added CsvWriter functionality

--- CsvWriter.java
+++ CsvWriter.java
@@ -1,13 +1,23 @@
+import java.io.*;
+
 public class CsvWriter {
     private String fileName;
+
     public CsvWriter(String fileName){
         this.fileName = fileName;
     }
 
-    public void writeTranscriptToCsv(){
-        /*
-        Functionality to write the Transcripts to a file
-        Interacts with the StudentRecordSystem where all the transcripts have been generated
-         */
+    public void writeToCsv(Object object){
+        try {
+
+            FileWriter out = new FileWriter(fileName);
+
+            out.write(object.toString());
+
+            out.close();
+        }
+        catch(IOException e){
+            throw new RecordSystemException("File output issue!");
+        }
     }
 }
