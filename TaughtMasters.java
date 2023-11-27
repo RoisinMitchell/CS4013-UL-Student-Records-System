77441a81ab0974cd2bc1e40fb2a5c4acd79ff15a
Gary3c <150540901+Gary3c@users.noreply.github.com>
2023-11-27 14:50:21 +0000

    Update and rename TaughtMasters.java to TaughtMaster.java

--- TaughtMasters.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class TaughtMasters extends MasterProgramme {
-    
-
-    public TaughtMasters(ArrayList<Module> modules, String programmeCode, String programmeName, int duration, int credits) {
-        super(modules, programmeCode, programmeName, duration, credits);
-    }
-}
