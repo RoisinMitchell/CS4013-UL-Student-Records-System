7c9e8974cdfb1813e141758107ef2cf18515ed1e
Jinggggggg <135239199+Jinggggggg@users.noreply.github.com>
2023-11-27 14:34:02 +0000

    Update new classes

--- StructuralPhd.java
+++ StructuralPhd.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
-public class TradtionalPhd extends PhdProgramme {
+public class StructuralPhd extends PhdProgramme {
 
-    public TradtionalPhd(ArrayList<Module> modules, String programmeCode, String programmeName, int duration,
+    public StructuralPhd(ArrayList<Module> modules, String programmeCode, String programmeName, int duration,
             int credits) {
         super(modules, programmeCode, programmeName, duration, credits);
         //TODO Auto-generated constructor stub
