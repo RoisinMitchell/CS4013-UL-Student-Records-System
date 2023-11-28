ebfd78d6550e673d713360231383308835975239
Jinggggggg <135239199+Jinggggggg@users.noreply.github.com>
2023-11-28 14:01:44 +0000

    Added hierarchy classes

--- StructuredPhd.java
+++ StructuredPhd.java
@@ -2,16 +2,49 @@ import java.util.ArrayList;
 
 public class StructuredPhd extends PhdProgramme {
 
+    private boolean thesisPassOrFail;
+    private int ECTSCredits;
+
+
+    // 1st class houers
     public StructuredPhd(ArrayList<Module> modules, String programmeCode, String programmeName, int duration,
-            int credits) {
+            int credits, boolean thesisPassOrFail, int ECTSCredits) {
         super(modules, programmeCode, programmeName, duration, credits);
-        //TODO Auto-generated constructor stub
+        this.thesisPassOrFail = thesisPassOrFail;
+        this.ECTSCredits = ECTSCredits;
     }
 
     
-    public boolean calculateProgression(){
-        
+    public boolean calculateProgression(Transcript transcript){
+        // check 
+        // QCA
+        // thesis
+        if(thesisPassOrFail && transcript.getCumulativeQCA() > 2.0) {
+            return true;
+        }
         return false;
         
     }
+
+
+    public boolean isThesisPassOrFail() {
+        return thesisPassOrFail;
+    }
+
+
+    public void setThesisPassOrFail(boolean thesisPassOrFail) {
+        this.thesisPassOrFail = thesisPassOrFail;
+    }
+
+
+    public int getECTSCredits() {
+        return ECTSCredits;
+    }
+
+
+    public void setECTSCredits(int eCTSCredits) {
+        ECTSCredits = eCTSCredits;
+    }
+
+
 }
